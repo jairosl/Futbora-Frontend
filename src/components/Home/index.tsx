c493a3c62c822ffa66f118289d8e32b5cbf8234d
jairosl <jairo.lima@aluno.uepb.edu.br>
2022-07-19 15:38:28 -0300

    feat: eat: conect to backend

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,10 +1,32 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import jogador from '../../assets/jogador.png';
 import logo from '../../assets/logo.png';
 import player from '../../assets/player.svg';
+import { api } from '../../services/api';
 import styles from './styles.module.scss';
+
 export function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await api.post('/session', { email, password });
+      alert('Usuário encontrado seguir para a pagina de partidas');
+    } catch (e: any) {
+      if (e.response.status === 404) {
+        alert('usuario não encontrado');
+      }
+      if (e.response.status === 400) {
+        alert('email ou senha invalido');
+      }
+      return;
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.left}>
@@ -16,24 +38,40 @@ export function Home() {
         </div>
       </div>
       <div className={styles.right}>
-        <form className={styles.cardLogin}>
+        <form className={styles.cardLogin} onSubmit={handleSubmit}>
           <div className={styles.logoContainer}>
             <img src={logo} alt="Logo" />
           </div>
           <div className={styles.textfield}>
             <label htmlFor="user">Usuário: </label>
-            <input type="text" name="user" placeholder="Usuário" />
+            <input
+              type="text"
+              name="user"
+              placeholder="Usuário"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
           </div>
           <div className={styles.textfield}>
             <label htmlFor="password">Senha: </label>
-            <input type="password" name="password" placeholder="Senha" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </div>
           <Link to={'/signUp'} className={styles.linkPassword}>
             <p>
               <a>Esqueceu sua senha? </a>
             </p>
           </Link>
-          <button className={styles.btn}>Login</button>
+          <button className={styles.btn} type="submit">
+            Login
+          </button>
           <Link to={'/signUp'} className={styles.link}>
             <p>
               <a>Criar conta</a>
--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,10 +1,44 @@
+import React, { useState } from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import jogador from '../../assets/jogador.png';
 import logo from '../../assets/logo.png';
+import { api } from '../../services/api';
 import styles from './styles.module.scss';
+
 export function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert('as senhas não são iguais');
+      return;
+    }
+    try {
+      await api.post('/users', {
+        name,
+        password,
+        email,
+      });
+      navigate('/');
+      alert('usuario criado com sucesso');
+      return;
+    } catch (e: any) {
+      if (e.response.status === 400) {
+        alert('Usuario já existe');
+        return;
+      }
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -16,27 +50,57 @@ export function SignUp() {
           </Link>
         </nav>
         <main className={styles.main}>
-          <form className={styles.cardLogin}>
+          <form className={styles.cardLogin} onSubmit={handleSubmit}>
             <div className={styles.logoContainer}>
               <img src={logo} alt="Logo" />
             </div>
             <div className={styles.textfield}>
-              <label htmlFor="user">Usuário: </label>
-              <input type="text" name="user" placeholder="Usuário" />
+              <label htmlFor="user">Nome: </label>
+              <input
+                type="text"
+                name="user"
+                placeholder="nome"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
             </div>
             <div className={styles.textfield}>
               <label htmlFor="user">Email: </label>
-              <input type="email" name="email" placeholder="E-mail" />
+              <input
+                type="email"
+                name="email"
+                placeholder="E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
             </div>
             <div className={styles.textfield}>
               <label htmlFor="password">Senha: </label>
-              <input type="password" name="password" placeholder="Senha" />
+              <input
+                type="password"
+                name="password"
+                placeholder="Senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
             </div>
             <div className={styles.textfield}>
               <label htmlFor="password">Confirmar Senha: </label>
-              <input type="password" name="password" placeholder="Senha" />
+              <input
+                type="password"
+                name="password"
+                placeholder="Senha"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
             </div>
-            <button className={styles.btn}>Criar Conta</button>
+            <button className={styles.btn} type="submit">
+              Criar Conta
+            </button>
             <div className={styles.imageContainer}>
               <img src={jogador} alt="imagem Jogador" />
             </div>
